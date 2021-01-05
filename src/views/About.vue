08b9868c33ee0b0fd46ebc1b031fcc41f0e85598
Afandy Ng <ng.afandy@gmail.com>
2021-01-05 12:46:31 -0500

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, nobis expedita, deleniti eius aperiam aliquam ipsa quos velit sit dolorum porro distinctio dolorem? Alias ipsa tempora laudantium dolorem quam ad omnis facere maiores sunt cum eos blanditiis quaerat unde, in ducimus? A maxime repellat aut eos fugiat atque, voluptatum laboriosam?</p>
+  </div>
+</template>
